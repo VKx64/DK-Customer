ddf6377fbbf9eea9dfb7e1ab5885985a682bf537
VKx64 <k-developer@zofky.com>
2025-05-28 12:35:17 +0800

    pre-deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
-    domains: ['db-daikin.07130116.xyz', 'zxky'],
+    domains: ['db-daikin.07130116.xyz'],
   },
 };
 
